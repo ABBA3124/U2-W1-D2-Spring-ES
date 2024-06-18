673b0b02749a17d7abad94ac8c9c6de853d905f9
ABBA3124 <davideabbadessa73@gmail.com>
2024-06-18 13:14:01 +0200

    create entities
    Tavolo
    Ordine

--- src/main/java/davideabbadessa/U2_W1_D1_Spring_ES/entities/Ordine.java
+++ src/main/java/davideabbadessa/U2_W1_D1_Spring_ES/entities/Ordine.java
@@ -1,4 +1,27 @@
 package davideabbadessa.U2_W1_D1_Spring_ES.entities;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ordine {
+    private int numeroOrdine;
+    private String stato; // in corso, pronto, servito    // valuterò dopo se usare enum
+    private int numeroCoperti;
+    private LocalDateTime oraAcquisizione;
+    private List<ProvaAstratta> elementiMenu;
+    private double importoTotale;
+    private Tavolo tavolo;
+
+
+    public Ordine(int numeroOrdine, String stato, int numeroCoperti, LocalDateTime oraAcquisizione, Tavolo tavolo) {
+        this.numeroOrdine = numeroOrdine;
+        this.stato = stato;
+        this.numeroCoperti = numeroCoperti;
+        this.oraAcquisizione = oraAcquisizione;
+        this.elementiMenu = new ArrayList<>();
+        this.tavolo = tavolo;
+    }
+
+
 }
--- src/main/java/davideabbadessa/U2_W1_D1_Spring_ES/entities/Tavolo.java
+++ src/main/java/davideabbadessa/U2_W1_D1_Spring_ES/entities/Tavolo.java
@@ -1,4 +1,48 @@
 package davideabbadessa.U2_W1_D1_Spring_ES.entities;
 
 public class Tavolo {
+    private int numero;
+    private int numeroCoperti;
+    private boolean occupato;
+
+
+    public Tavolo(int numero, int numeroCoperti, boolean occupato) {
+        this.numero = numero;
+        this.numeroCoperti = numeroCoperti;
+        this.occupato = occupato;
+    }
+
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getNumeroCoperti() {
+        return numeroCoperti;
+    }
+
+    public void setNumeroCoperti(int numeroCoperti) {
+        this.numeroCoperti = numeroCoperti;
+    }
+
+    public boolean isOccupato() {
+        return occupato;
+    }
+
+    public void setOccupato(boolean occupato) {
+        this.occupato = occupato;
+    }
+
+    @Override
+    public String toString() {
+        return "Tavolo{" +
+                "numero=" + numero +
+                ", numeroCoperti=" + numeroCoperti +
+                ", occupato=" + occupato +
+                '}';
+    }
 }
